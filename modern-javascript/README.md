8a5e0fdb5c4441a49e3f8fbfa5a47b0418e272a3
nguyentuan1696 <nguyentuan1696@gmail.com>
2021-01-05 20:44:39 +0700

    modern javascript

---
+++ modern-javascript/README.md
@@ -0,0 +1,7 @@
+## Modern Javascript
+
+Opimize Javascript bundle with webpack 5
+
+Have 2 type bundles:
+- Legacy js bundle for old-browser
+- Modern js bundle for modern-browser
--- vanilla-js-projects/expanding-cards/script.js
+++ vanilla-js-projects/expanding-cards/script.js
@@ -12,3 +12,14 @@ function removeActiveClasses() {
         panel.classList.remove('active')
     })
 }
+
+/*
+1. Khi click vao 1 panel
+2. Chay vong lap tat ca cac items de xoa trang thai active
+3. Add trang thai active vao panel vua click
+
+function removeActiveClasses
+1. Chay vong lap tat ca phan tu 
+2. Xoa class actve o tat ca cac phan tu
+
+*/
