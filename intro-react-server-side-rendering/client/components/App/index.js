43213ef7727af492da23ca04383abcbb9e080a40
nguyentuan1696 <nguyentuan1696@gmail.com>
2021-01-11 22:40:08 +0700

    add intro react server side rendering

---
+++ intro-react-server-side-rendering/client/components/App/index.js
@@ -0,0 +1,13 @@
+// ./client/components/App/index.js
+
+import React from 'react'
+import style from './style'
+
+const App = () => (
+  <>
+    <div className={style.app}>Hello World</div>
+    <button onClick={(e) => alert('Hello You!')}>Say Hello Back!</button>
+  </>
+)
+
+export default App
