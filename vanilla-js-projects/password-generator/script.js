775142dc69621dd4885c6be656c97ce12e0f13a3
nguyentuan1696 <nguyentuan1696@gmail.com>
2021-01-12 21:41:06 +0700

    add comment

--- vanilla-js-projects/password-generator/script.js
+++ vanilla-js-projects/password-generator/script.js
@@ -69,14 +69,19 @@ function generatePassword(lower, upper, number, symbol, length) {
   return finalPassword
 }
 
+// 24 + 97 = 121 => y
 function getRandomLower() {
   return String.fromCharCode(Math.floor(Math.random() * 26) + 97)
 }
 
+
+// English alphabet letters: 26
+// 25 + 65 = 90 => Z
 function getRandomUpper() {
   return String.fromCharCode(Math.floor(Math.random() * 26) + 65)
 }
-
+// Number: 0 - 10, 10 numbers
+// 1 + 48 => 2
 function getRandomNumber() {
   return String.fromCharCode(Math.floor(Math.random() * 10) + 48)
 }
@@ -85,3 +90,14 @@ function getRandomSymbol() {
   const symbols = '!@#$%^&*(){}[]=<>/,.'
   return symbols[Math.floor(Math.random() * symbols.length)]
 }
+
+console.log(getRandomUpper())
+
+
+// Read more String.fromCharCode()
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/fromCharCode
+// Convert a Unicode number into a character
+// English alphabet letters: 26
+// Greater than 97 return lowercase letter 
+// Greater than 65 return uppercase letter
+// Greater than 48 return number
