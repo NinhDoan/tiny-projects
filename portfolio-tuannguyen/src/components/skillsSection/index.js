13ba34d6f53786014847288febf77e3aaeb9b3dc
nguyentuan1696 <nguyentuan1696@gmail.com>
2021-02-17 00:28:30 +0700

    update

--- portfolio-tuannguyen/src/components/skillsSection/index.js
+++ portfolio-tuannguyen/src/components/skillsSection/index.js
@@ -5,15 +5,20 @@ const SkillsSection = () => {
   return (
     <>
       <div class={style.skills}>
-        <h2 class={`heading-section`}>My skills</h2>
+        <div class={style.heading}>
+          <h2 class={`heading-section`}>My skills</h2>
+        </div>
         <div class={style.skills__content}>
           <p>
-            <span>I have a solid understanding on </span>
-            <span>HTML, CSS, Javascript</span>
+            <span class={style.para_sub}>
+              {' '}
+              I have a solid understanding on{' '}
+            </span>
+            <span class={style.para_main}>HTML, CSS, Javascript</span>
           </p>
           <p>
-            <span>Basic knowledge on</span>
-            <span>ReactJS, NodeJS</span>
+            <span class={style.para_sub}>Basic knowledge on</span>
+            <span class={style.para_main}>ReactJS, NodeJS</span>
           </p>
         </div>
       </div>
