bdfbd0c87f8a03853186565b2e5d27f0820b97f8
nguyentuan1696 <nguyentuan1696@gmail.com>
2021-02-25 11:19:49 +0700

    update

---
+++ shinkme-getlink/src/components/Form/index.js
@@ -0,0 +1,16 @@
+import { h } from 'preact'
+import { useState } from 'preact/hooks'
+import axios from 'redaxios'
+
+const Form = () =>
+{
+  const [data, setData] = useState('')
+
+  
+
+  return (
+    <div>tuan</div>
+  )
+}
+
+export default Form
---
+++ shinkme-getlink/src/components/app.js
@@ -0,0 +1,11 @@
+import { h } from 'preact';
+import Form from '../components/Form'
+
+const App = () =>
+{
+	<>
+	<Form />
+	</>
+}
+
+export default App;
