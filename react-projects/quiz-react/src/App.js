e2842f871d495dd6baf45159ab9f879361c05f42
nguyentuan1696 <nguyentuan1696@gmail.com>
2021-03-09 22:05:17 +0700

    add quiz-react

---
+++ react-projects/quiz-react/src/App.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import { useGlobalContext } from './context'
+
+import SetupForm from './SetupForm'
+import Loading from './Loading'
+import Modal from './Modal'
+function App() {
+  const { waiting, loading, questions, index, correct } = useGlobalContext()
+  if (waiting) {
+    return <SetupForm />
+  }
+
+  if (loading) {
+    return <Loading />
+  }
+  const { question, incorrect_answers, correct_answer } = questions[0]
+  const answers = [...incorrect_answers, correct_answer]
+  return (
+    <>
+      <main>
+        <Modal />
+        <section className="quiz">
+          <p className="correct-answers">
+            correct answers: {correct}/{index}
+          </p>
+          <article className="container">
+            
+          </article>
+      </section>
+      </main>
+    </>
+  )
+}
+
+export default App
