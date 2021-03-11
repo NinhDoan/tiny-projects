e0b861904a10efa02e22fa084c34e29ab0677cb6
nguyentuan1696 <nguyentuan1696@gmail.com>
2021-03-11 16:13:08 +0700

    update

--- react-projects/react-shop/src/components/Hero.js
+++ react-projects/react-shop/src/components/Hero.js
@@ -5,7 +5,29 @@ import heroBcg from '../assets/hero-bcg.jpeg'
 import heroBcg2 from '../assets/hero-bcg-2.jpeg'
 
 const Hero = () => {
-  return <h4>hero</h4>
+  return (
+    <Wrapper className='section-center'>
+      <article className='content'>
+        <h1>
+          design your <br />
+          comfort zone
+        </h1>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Mollitia a
+          minima assumenda voluptate facere quam animi totam recusandae officiis
+          sit!
+        </p>
+        <Link to='/product' className='btn hero-btn'>
+          shop now
+        </Link>
+      </article>
+
+      <article className='img-container'>
+        <img src={heroBcg} alt='nice table' className='main-img'></img>
+        <img src={heroBcg2} alt='nice table' className='accent-img'></img>
+      </article>
+    </Wrapper>
+  )
 }
 
 const Wrapper = styled.section`
--- react-projects/react-shop/src/pages/AboutPage.js
+++ react-projects/react-shop/src/pages/AboutPage.js
@@ -4,7 +4,25 @@ import { PageHero } from '../components'
 import aboutImg from '../assets/hero-bcg.jpeg'
 
 const AboutPage = () => {
-  return <h4>about page</h4>
+  return (
+    <main>
+      <PageHero title='About' />
+      <Wrapper className='page section section-center'>
+        <img src={aboutImg} alt='nice desk'></img>
+        <article>
+          <div className='title'>
+            <h2>our story</h2>
+            <div className='underline'></div>
+          </div>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquid,
+            iure atque. Modi asperiores cumque obcaecati non id ullam nobis
+            dolores.
+          </p>
+        </article>
+      </Wrapper>
+    </main>
+  )
 }
 
 const Wrapper = styled.section`
