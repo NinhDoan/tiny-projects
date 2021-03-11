6311955af5373c49cef01d417476e533dce008a4
nguyentuan1696 <nguyentuan1696@gmail.com>
2021-03-12 01:39:46 +0700

    update

--- react-projects/react-shop/src/context/products_context.js
+++ react-projects/react-shop/src/context/products_context.js
@@ -20,6 +20,9 @@ const initialState = {
   products_error: false,
   products: [],
   featured_products: [],
+  single_product_loading: false,
+  single_product_error: false,
+  single_product: {},
 }
 
 const ProductsContext = React.createContext()
@@ -44,7 +47,18 @@ export const ProductsProvider = ({ children }) => {
       const products = response.data
       dispatch({ type: GET_PRODUCTS_SUCCESS, payload: products })
     } catch (error) {
-      dispatch({ type: GET_PRODUCTS_ERROR})
+      dispatch({ type: GET_PRODUCTS_ERROR })
+    }
+  }
+
+  const fetchSingleProduct = async (url) => {
+    dispatch({ type: GET_SINGLE_PRODUCT_BEGIN })
+    try {
+      const response = await axios.get(url)
+      const singleProduct = response.data
+      dispatch({ type: GET_SINGLE_PRODUCT_SUCCESS })
+    } catch (error) {
+      dispatch({ type: GET_SINGLE_PRODUCT_ERROR })
     }
   }
 
