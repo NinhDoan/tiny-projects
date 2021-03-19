ee1902e354e9272d15fadd82e0928f69620afffb
nguyentuan1696 <nguyentuan1696@gmail.com>
2021-03-19 19:13:26 +0700

    add site config

---
+++ homepage-test/site.config.js
@@ -0,0 +1,40 @@
+export const config = {
+  title: 'HomePage Test',
+  tagline: 'A sample test',
+  url: '#',
+  heroTitle: 'happypoints',
+  heroBackgroundUrl: '',
+
+  headerMenu: [
+    { label: 'new arrivals', link: '#' },
+    { label: 'boots', link: '#' },
+    { label: 'flats', link: '#' },
+    { label: 'heels', link: '#' },
+    { label: 'sandals', link: '#' },
+  ],
+  customerCare: [
+    { label: 'ordering & payment', link: '#' },
+    { label: 'shipping & tracking', link: '#' },
+    { label: 'returns', link: '#' },
+    { label: 'internatinal', link: '#' },
+    { label: 'contacts', link: '#' },
+  ],
+  gallery: [
+    { src: '#', label: 'anh-1' },
+    { src: '#', label: 'anh-2' },
+    { src: '#', label: 'anh-3' },
+    { src: '#', label: 'anh-4' },
+    { src: '#', label: 'anh-4' },
+    { src: '#', label: 'anh-5' },
+  ],
+  footer: [
+    { label: 'company info', link: '#' },
+    { label: 'privacy policy (updated 2017)', link: '#' },
+    { label: 'terms of use', link: '#' },
+    {
+      label: 'california transparency in supply chains act statement',
+      link: '#',
+    },
+    { label: 'sitemap', link: '#' },
+  ],
+}
