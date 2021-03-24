6d1a99e043290477f0c6dd5db049d559eb909495
nguyentuan1696 <nguyentuan1696@gmail.com>
2021-03-24 23:31:03 +0700

    update

---
+++ vue-projects/vue-shop/src/pages/index.js
@@ -0,0 +1,16 @@
+import Home from '@pages/HomePage'
+import About from '@pages/AboutPage'
+import Error from '@pages/ErrorPage'
+import Product from '@pages/ProductPage'
+import SingleProduct from '@pages/SingleProductPage'
+import Checkout from '@pages/CheckoutPage'
+
+export
+{
+  Home,
+  About,
+  Error,
+  Product,
+  SingleProduct,
+  Checkout
+}
